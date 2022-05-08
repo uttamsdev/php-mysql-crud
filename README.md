6313e963632bf38243332b2adf3173c9d7e498f5
uttamsaha <uttamsaha982@gmail.com>
2022-05-08 12:49:04 +0600

    databse added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # PHP CRUD 
-This is a simple php crud operation. CRUD is an acronym for Create, Read, Update, and Delete. CRUD operations are basic data manipulation for database. This  simple PHP CRUD application to perform all these operations on a MySQL database table at one place.
+This is a simple php crud operation. CRUD is an acronym for Create, Read, Update, and Delete. CRUD operations are basic data manipulation for database. This  simple PHP CRUD application to perform all these operations on a MySQL database table at one place. Database included in the files section.
 ## Features
 * Read Data
 * Insert data
---
+++ stdinfo.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 08, 2022 at 08:47 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stdinfo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(10) NOT NULL,
+  `stdname` text NOT NULL,
+  `stdreg` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `stdname`, `stdreg`) VALUES
+(3, 'komol saha', '22'),
+(4, 'robin khanx', '5555555'),
+(7, 'Uttam Kumar Saha', '201');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
